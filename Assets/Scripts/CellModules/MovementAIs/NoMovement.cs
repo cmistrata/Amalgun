f58e444d48d93ddd279bb0a1802d852a50048c51
nmistrata <nmistrata@gmail.com>
2024-04-19 12:34:29 -0500

    Create NoMovement movement type for use by the player

---
+++ Assets/Scripts/CellModules/MovementAIs/NoMovement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoMovement : MovementBase
+{
+    public override void ApplyMovement(Rigidbody rb, float timePassed)
+    {
+
+    }
+}
